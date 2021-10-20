6f538ac6a1cde7e28b1d56bf7f6722a49db458a0
Strong Jimmy <76681700+fatcatorange@users.noreply.github.com>
2021-10-20 18:28:43 +0800

    Add files via upload

--- error/message_box.cpp
+++ error/message_box.cpp
@@ -1,12 +1,21 @@
 #include<cstdio>
 #include<windows.h>
+void checkOutput(int output);
 
 void error()
 {
-	MessageBox(NULL,"error!","error!",4);
-	error();
+	int output;
+	output=MessageBox(NULL,"error!","error!",4);
+	checkOutput(output);
 }
 
+void checkOutput(int output)
+{
+	if(output==6)
+		error();
+	if(output==7)
+		error();
+} 
 int main()
 {
 	FreeConsole();
